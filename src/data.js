542b47b5ee843c60396978583d8f3c9c6f974749
Yahor Neverouski <hardstyle6537@gmail.com>
2020-04-06 16:03:26 +0300

    fix: add name to object && export obj

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-export default {
+const keys = {
   ruUpperCase: ['Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+',
     'Backspace', 'Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '/', 'Del',
     'CapsLock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter', 'Shift',
@@ -19,7 +19,7 @@ export default {
 
   engLowerCase: ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ' - ', '=',
     'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del',
-    'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter', 'Shift', 'z',
+    'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', `'`, 'Enter', 'Shift', 'z',
     'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '&#x25b2; ', 'Shift', 'Ctrl', 'Win', 'Alt',
     ' ', 'Alt', '&#x25c0; ', '&#x25bc; ', '&#x25b6; ', 'Ctrl'],
 
@@ -32,3 +32,5 @@ export default {
     'ArrowUp', 'ShiftRight', 'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight',
     'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'],
 };
+
+export default keys;
